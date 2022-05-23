3aecc0b9c669bb506e84000fd4d7ebcae8f36a6c
Bryan Kim <bkim0826@gmail.com>
2022-05-23 11:43:01 -0400

    removed node

--- Demo/node_modules/@mui/material/legacy/Backdrop/backdropClasses.js
+++
@@ -1,6 +0,0 @@
-import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
-export function getBackdropUtilityClass(slot) {
-  return generateUtilityClass('MuiBackdrop', slot);
-}
-var backdropClasses = generateUtilityClasses('MuiBackdrop', ['root', 'invisible']);
-export default backdropClasses;
