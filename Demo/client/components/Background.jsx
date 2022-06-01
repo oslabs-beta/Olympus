0749177ec49285e017c8b8a885d868f8188463ee
Bryan Kim <bkim0826@gmail.com>
2022-06-01 13:01:16 -0400

    Olympus background image

---
+++ Demo/client/components/Background.jsx
@@ -0,0 +1,13 @@
+import Olympus from '../assets/Olympus.png'
+import React from 'react'
+import '../styles/Background.css'
+
+const Background = () => {
+  return (
+    <div>
+        <img className='demo-background' src={Olympus} /> 
+    </div>
+  )
+}
+
+export default Background
