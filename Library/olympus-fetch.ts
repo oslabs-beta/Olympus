7aa681228f68a82a9b01077bbcff97f2b8c14c9c
Bryan Kim <bkim0826@gmail.com>
2022-06-25 14:07:11 -0400

    Revert

--- Library/olympus-fetch.ts
+++ Library/olympus-fetch.ts
@@ -4,7 +4,7 @@ async function Olympus(options:any) {
     let query = options.body;
     
     // function definition of helper function to set expiration on local storage items
-    function setWithExpiration(key, value, ttl) {
+    function setWithExpiration(key:string, value:string, ttl:number) {
         const now = new Date();
         // 'item' is an object which contains the original value as well as the time when it's supposed to expire
         const item = {
@@ -16,7 +16,7 @@ async function Olympus(options:any) {
     };
 
     // function definition of helper function to get item from local storage while considering expiration
-    function getWithExpiration(key) {
+    function getWithExpiration(key:string) {
         // retrieve value of argument key in localStorage
         const itemStr = localStorage.getItem(key);
         // if the item doesn't exist, return null
@@ -50,7 +50,7 @@ async function Olympus(options:any) {
             // else create it in local storage
             else {
                 // make async request to olympus endpoint passing in options object
-                let serverResponse = await fetch('http://localhost:3000/olympus', options);
+                let serverResponse:any = await fetch('http://localhost:3000/olympus', options);
                 // json the response
                 serverResponse =  await serverResponse.json();
                 // stringify the response
